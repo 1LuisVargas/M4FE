07a9d21876c643ea56fc835c2bb7f3a764429bf7
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-09-22 22:31:30 -0500

    Added alt to the image, and created new interface for card props

--- front/src/components/ProductCard.tsx
+++ front/src/components/ProductCard.tsx
@@ -1,10 +1,14 @@
 import IProduct from "@/interfaces/IProduct";
 
-export default function ProductCard(product: IProduct) {
+interface IProductCardProps {
+    product: IProduct;
+}
+
+export default function ProductCard({product}: IProductCardProps) {
   return (
     <div>
         <h2>{product.name}</h2>
-        <img src={product.image} alt="" />
+        <img src={product.image} alt={`Image of ${product.name}`} />
         <p>{product.description}</p>
         <p>{product.price}</p>
       </div>
