df5bf7e1cfd83b73e0fb5a9a61d4ac465d9b0cb8
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-09-24 21:41:56 -0500

    Created footer. Made minor changes to navbar. Created not found. Setup layout

---
+++ front/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const notFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+        </div>
+    );
+}
+
+export default notFound;
--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import NAVITEMS from "@/helpers/NavBarItems";
 
 const NavBar = () => {
     return (
-        <nav>
+        <nav className="width-100 flex justify-between items-center">
             <ul>
                 <li><Link href={NAVITEMS.HOME}>Home</Link></li>
                 <li><Link href={NAVITEMS.DASHBOARD}>Dashboard</Link></li>
