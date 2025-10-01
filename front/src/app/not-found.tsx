60b7d70a15a4eedecaa50fb2d5fd85f2836b04e4
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-09-30 20:29:54 -0500

    More styling changes to layout and not found

--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -1,9 +1,13 @@
 const notFound = () => {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-3xl m-4 text-center">Opps, looks like you tried to reach a page that doesn&apos;t exist. <br />404</h1>
-        </div>
-    );
-}
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl m-4 text-center">
+        Opps, looks like you tried to reach a page that doesn&apos;t exist.{" "}
+        <br />
+        404
+      </h1>
+    </div>
+  );
+};
 
 export default notFound;
