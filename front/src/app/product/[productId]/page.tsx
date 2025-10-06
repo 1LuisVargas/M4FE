73f05b2ec55ae0723c307d8f5df3ec5e342095f5
Luis Vargas <luisvargas98@hotmail.com>
2025-10-06 15:54:03 -0500

    Added functionality to get all products and product by ID

--- front/src/app/product/[productId]/page.tsx
+++ front/src/app/product/[productId]/page.tsx
@@ -1,16 +1,38 @@
-const productDetails = async ({
-  params,
-}: {
-  params: Promise<{ productId: string }>;
-}) => {
-  const { productId } = await params; // Getting the product id
+"use client";
+import IProduct from "@/interfaces/IProduct";
+import { getProductById } from "@/services/products.services";
+import { useParams } from "next/navigation";
+import { useEffect, useState} from "react";
+import Image from "next/image";
+
+const ProductDetails = () => {
+  const params = useParams();
+  const [product, setProduct] = useState<IProduct>();
+
+  useEffect(() => {
+    const fetchProductById = async () => {
+      const temporaryProduct = await getProductById(params.productId as string);
+      setProduct(temporaryProduct);
+    };
+
+    fetchProductById();
+  }, [params.productId]);
 
   return (
-    <div>
-      <h1 className="text-3xl">This is the product page for product:</h1>
-      <p className="text-2xl">{productId}</p>
+    <div className="flex items-center flex-col">
+      <h1 className="h1">This is the product page for product:</h1>
+      <p className="text-2xl">{product?.name}</p>
+      <Image
+        className="m-4"
+        src={product?.image}
+        alt={`Image of ${product?.name}`}
+        width={200}
+        height={200}
+      />
+      <p className="text-2xl">{product?.description}</p>
+      <p className="text-2xl">{product?.price}</p>
     </div>
   );
 };
 
-export default productDetails;
+export default ProductDetails;
--- front/src/services/products.services.ts
+++ front/src/services/products.services.ts
@@ -1,11 +1,20 @@
 import IProduct from "@/interfaces/IProduct";
 
 export const getAllProducts = async () => {
-    try {
-        const data = await fetch("http://localhost:3005/products");
-        const products: IProduct[] = await data.json();
-        return products;
-    } catch (error) {
-        throw new Error("Failed to fetch products");
-    }
-}
+  try {
+    const data = await fetch("http://localhost:3005/products");
+    const products: IProduct[] = await data.json();
+    return products;
+  } catch (error) {
+    throw new Error(`Error fetching products: ${error}`);
+  }
+};
+
+export const getProductById = async (id: string) => {
+  try {
+    const products = await getAllProducts();
+    return products.find((product: IProduct) => product.id === parseInt(id));
+  } catch (error) {
+    throw new Error(`Error fetching the requested product: ${error}`);
+  }
+};
