1614f215660c63a6f17762fc6aef281fd9fc906d
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-10-10 14:37:54 -0500

    Minor styling change

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -1,7 +1,7 @@
 const home = () => {
     return (
         <div className="flex justify-center items-center m-4">
-            <h1 className="text-3xl">Welcome to our store! We have the best products at great prices!</h1>
+            <h1 className="h1">Welcome to our store! We have the best products at great prices!</h1>
         </div>
     )
 }
