3eb8f0768038a385c1695bfb1fa229b70d485306
Luis Vargas <luisvargas98@hotmail.com>
2025-10-29 08:47:19 -0500

    Removed previous auth implementation to clean up code

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import ClientProvider from "@/components/ClientProvider";
+import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,11 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col bg-slate-400`}
       >
-        <ClientProvider>
+        <AuthProvider>
           <NavBar />
           <main className="flex-1">{children}</main>
           <Footer />
-        </ClientProvider>
+        </AuthProvider>
       </body>
     </html>
   );
