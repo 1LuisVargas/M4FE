9a407d236150ff7440369ef1e85cfd50e6148c42
Luis Vargas <luisvargas98@hotmail.com>
2025-10-31 09:49:58 -0500

    Overhaul the product details page to better match the new service logic

--- front/src/app/product/[productId]/page.tsx
+++ front/src/app/product/[productId]/page.tsx
@@ -2,37 +2,35 @@ import IProduct from "@/interfaces/IProduct";
 import { getProductById } from "@/services/products.services";
 import Image from "next/image";
 
-const ProductDetails = ({
+const ProductDetails = async ({
   params,
 }: {
   params: { [productId: string]: string }; // Defining the type of params
 }) => {
   const { productId } = params;
-  let product = {} as IProduct;
+  let product: IProduct;
 
   try {
-    product = getProductById(productId);
-  } catch (error) {
+    product = await getProductById(productId);
+    return (
+      <div className="flex items-center flex-col">
+        <h1 className="h1">This is the product page for product:</h1>
+        <p className="h2">{product.name}</p>
+        <Image
+          className="m-4"
+          src={product.image}
+          alt={`Image of ${product.name}`}
+          width={200}
+          height={200}
+        />
+        <p className="text-2xl">{product.description}</p>
+        <p className="text-2xl">{product.price}</p>
+      </div>
+    );
+  } catch (error) { //TODO: This not working as intended. Pending further review
+    alert(error);
     console.log(error);
   }
-
-  //TODO: FINISH VIEWING THE 6TH CLASS
-
-  return (
-    <div className="flex items-center flex-col">
-      <h1 className="h1">This is the product page for product:</h1>
-      <p className="text-2xl">{product?.name}</p>
-      <Image
-        className="m-4"
-        src={product?.image}
-        alt={`Image of ${product?.name}`}
-        width={200}
-        height={200}
-      />
-      <p className="text-2xl">{product?.description}</p>
-      <p className="text-2xl">{product?.price}</p>
-    </div>
-  );
 };
 
 export default ProductDetails;
