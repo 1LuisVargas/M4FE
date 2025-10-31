70e36390cdbcbac24ccfb6114717e936409e6f3b
Luis Vargas <luisvargas98@hotmail.com>
2025-10-31 10:33:13 -0500

    Finished the cart context creation

--- front/src/contexts/CartContext.tsx
+++ front/src/contexts/CartContext.tsx
@@ -2,11 +2,13 @@
 
 import IProduct from "@/interfaces/IProduct";
 import { createContext, useEffect, useState } from "react";
+import { useAuth } from "./AuthContext";
+import { useRouter } from "next/navigation";
 
 type CartContextType = {
     cart: IProduct[], 
     addToCart: (product: IProduct) => void,
-    removeFromCart: (product: IProduct) => void,
+    removeFromCart: (productId: number) => void,
     clearCart: () => void,
     getTotalPrice: () => number,
     getTotalQuantity: () => number,
@@ -19,6 +21,8 @@ export const CartContext = createContext<CartContextType | null>(null);
 
 export const CartContextProvider = ({ children }: { children: React.ReactNode }) => {
     const [cart, setCart] = useState<IProduct[]>([]);
+    const { isAuthenticated } = useAuth();
+    const router = useRouter();
 
     useEffect(() => {
         const storedCart = localStorage.getItem(CART_KEY);
@@ -31,8 +35,43 @@ export const CartContextProvider = ({ children }: { children: React.ReactNode })
         localStorage.setItem(CART_KEY, JSON.stringify(cart));
     }, [cart]);
 
+    const addToCart = (product: IProduct) => {
+        if(!isAuthenticated){
+            alert("Please login to add items to your cart");
+            router.push("/login");
+        }
+
+        const existingProduct = cart.find((item) => item.id === product.id);
+        if (existingProduct) {
+            alert("Product already in cart. You can't have two of the same item in the cart.");
+            return;
+        }
+
+        setCart([...cart, product]);
+    };
+
+    const removeFromCart = (productId: number) => {
+        setCart(cart.filter((item) => item.id !== productId));
+    };
+
+    const clearCart = () => {
+        setCart([]);
+    };
+
+    const getTotalPrice = () => {
+        return cart.reduce((total, item) => total + item.price, 0);
+    };
+
+    const getTotalQuantity = () => {
+        return cart.length;
+    };
+
+    const getIds = () => {
+        return cart.map((item) => item.id);
+    };
+    
     return (
-        <CartContext.Provider value={null}>
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, getTotalPrice, getTotalQuantity, getIds }}>
             {children}
         </CartContext.Provider>
     );
