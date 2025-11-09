cba78707fa7300d5337d84ff42dc16be2f3b1bc9
Luis Vargas <luisvargas98@hotmail.com>
2025-11-08 23:49:36 -0500

    Added ngrok header to skip interstitial

--- front/src/services/orders.services.ts
+++ front/src/services/orders.services.ts
@@ -1,12 +1,13 @@
 import IOrder from "../interfaces/IOrder";
 
-export const getOrdersByUserId = async (token: string, ) => {
+export const getOrdersByUserId = async (token: string) => {
   const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/orders/`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
+      "ngrok-skip-browser-warning": "1",
     },
-  })
+  });
   if (!data.ok) {
     throw new Error(`Orders ${data.statusText}`);
   }
@@ -20,20 +21,20 @@ export const createOrder = async (token: string, products: number[]) => {
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
-      'ngrok-skip-browser-warning': '1'
+      "ngrok-skip-browser-warning": "1",
     },
     body: JSON.stringify({ products }),
-  }) 
+  });
 
   if (!data.ok) {
     throw new Error(`${data.statusText}`);
   }
   const order: IOrder = await data.json();
   return order;
-}
+};
 
 export const getOrderById = async (orderId: number, token: string) => {
   const orders = await getOrdersByUserId(token);
   const order = orders.find((order: IOrder) => order.id === orderId);
   return order;
-}
+};
